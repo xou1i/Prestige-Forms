bbbed221386a5d826bba4c47c9a302619de3bc88
Ghaith Ali <ghaithali206@gmail.com>
2025-11-19 16:34:26 +0300

    1

---
+++ src/script.svelte
@@ -0,0 +1,11 @@
+<script>
+
+// onclick hire me button scroll to contact section
+  function scrollToContact() {
+    const hireMeButton = document.getElementById('HIRE');
+    const contactSection = document.getElementById('contact');
+    if (contactSection === hireMeButton) {
+      contactSection.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+</script>
