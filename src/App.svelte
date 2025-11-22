49abbead5b9c5c8fea9ab78fdcf1854af0a5ae0c
Ghaith Ali <ghaithali206@gmail.com>
2025-11-22 12:28:42 +0300

    Refactor component structure: move Nav and Hero components to Home, add Login and Dashboard components, and create new Create components

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,33 @@
 <script>
-  import Nav from "./Dashboard/Nav.svelte";
-  import Hero from "./Dashboard/Hero.svelte";
+  // Home components
+  import Nav from "./Home/Nav.svelte"
+  import Hero from "./Home/Hero.svelte"
+
+  // login components
+  import NavLogin from "./Login/NavLogin.svelte";
+  import HeroLogin from "./Login/HeroLogin.svelte";
+
+// Dashboard components
+  import NavDash from "./Dashboard/NavDash.svelte";
+  import HeroDash from "./Dashboard/HeroDash.svelte";
+
+  // Create component
+  import NavCreate from "./Create/NavCreate.svelte";
+  import HeroCreate from "./Create/HeroCreate.svelte";
 </script>
 
 
 <Nav />
 <Hero />
-+
+
+<NavLogin />
+<HeroLogin />
+
+<NavDash />
+<HeroDash />
+
+<NavCreate />
+<HeroCreate />
 
 
 
