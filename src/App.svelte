85e523029ef680ff8471ca16630f6974f0557b6d
Ghaith Ali <ghaithali206@gmail.com>
2025-11-22 11:47:50 +0300

    Add navigation component and refactor App.svelte to use it

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,8 @@
 <script>
-  import Navbar from './components/Navbar.svelte';
-  import Hero from './sections/Hero.svelte';
-  import About from './sections/About.svelte';
-  import Skills from './sections/Skills.svelte';
-  import Projects from './sections/Projects.svelte';
-  import Experience from './sections/Experience.svelte';
-  import Contact from './sections/Contact.svelte';
-  import Footer from './components/Footer.svelte';
-
+  import Nav from "./Dashboard/Nav.svelte";
 </script>
 
-<Navbar />
-<Hero />
-<About />
-<Skills />
-<Projects />
-<Experience />
-<Contact />
-<Footer />
+
+<Nav />
 
 <main class="p-10"></main>
